85d2a432f7cdd69b8b1482f4a64a2da307aa4ebf
tobeyOguney <tobyogunbayo@gmail.com>
2020-01-28 14:54:05 +0100

    asdfadsf

--- application.py
+++ application.py
@@ -2,10 +2,10 @@
 from manage import app as application
 
 import logging
-    from logging import StreamHandler
-    file_handler = StreamHandler()
-    file_handler.setLevel(logging.WARNING)
-    application.logger.addHandler(file_handler)
+from logging import StreamHandler
+file_handler = StreamHandler()
+file_handler.setLevel(logging.WARNING)
+application.logger.addHandler(file_handler)
 
 if __name__ == "__main__":
     application.run()
