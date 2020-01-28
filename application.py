69e25923158847d2a813de2eb6b9d2f76d61cc83
tobeyOguney <tobyogunbayo@gmail.com>
2020-01-28 15:16:01 +0100

    asdfasdf

--- application.py
+++ application.py
@@ -1,11 +1,5 @@
 # wsgi.py
 from manage import app as application
 
-import logging
-from logging import StreamHandler
-file_handler = StreamHandler()
-file_handler.setLevel(logging.WARNING)
-application.logger.addHandler(file_handler)
-
 if __name__ == "__main__":
     application.run()
